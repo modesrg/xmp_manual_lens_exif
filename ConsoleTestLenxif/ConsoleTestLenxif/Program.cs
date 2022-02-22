67d910bbf4866c506404fc5c02bcc76b02882998
modesrg <modrodgal@outlook.com>
2022-02-22 18:18:37 +0100

    Commit Library port and Console Test

---
+++ ConsoleTestLenxif/ConsoleTestLenxif/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleTestLenxif
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Lenxif.Lenxif lenxif = new Lenxif.Lenxif();
+                string testPath = @"F:\RAW Laptop\2022\enero\02";
+                lenxif.UpdateManualLens(testPath);
+            }
+            catch (Exception ex)
+            {
+
+
+            }
+        }
+    }
+}
